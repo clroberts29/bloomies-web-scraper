5268573c937509b5266baf66d81def894350d07c
clroberts29 <50728923+clroberts29@users.noreply.github.com>
2019-06-28 12:43:52 -0400

    print coo

--- web_scraper.py
+++ web_scraper.py
@@ -8,26 +8,22 @@ import time
 api_key = os.environ.get("sample_page_source")
 
 
-print(type(sample_page_source))
 
+header = {'user-agent': 'Mozilla/5.0'}
 
-#
-#
-#header = {'user-agent': 'Mozilla/5.0'}
-#
 #website = 'https://www.bloomingdales.com/shop/product/burberry-icon-stripe-gauze-scarf?ID=3388848&CategoryID=1005369#fn=ppp%3Dundefined%26sp%3DNULL%26rId%3DNULL%26spc%3D366%26spp%3D2%26pn%3D1%7C4%7C2%7C366%26rsid%3Dundefined%26smp%3DmatchNone'
-##website = "https://www.google.com"
-#
-#
-#
-#raw_html = requests.get(website, headers=header)
-#
-#html = raw_html.content
-#html = open(html).read()
-#
-#soup = BeautifulSoup(html, 'html.parser')
-##(soup.prettify())
-#
-##print(soup.prettify())
-#
-#pricing = soup.final_all("div",{'class':'final-price'})
+website = "https://www.google.com"
+
+
+
+raw_html = requests.get(website, headers=header)
+
+html = raw_html.content
+html = open(html).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+soup.prettify()
+
+#print(soup.prettify())
+
+
